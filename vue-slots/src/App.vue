3b72a24de5c82b2942223a598f1a3c0f6af47d16
mayank-cyb <73458158+mayank-cyb@users.noreply.github.com>
2022-11-07 22:59:22 +0530

    vue slots practice

---
+++ vue-slots/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <AppButton @click="useMe">Use me</AppButton>
+  <AppButton>Like me</AppButton>
+  <AppButton>Buy me</AppButton>
+</template>
+
+<script>
+import AppButton from './components/AppButton.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppButton,
+  },
+  methods: {
+    useMe() {
+      console.log("Clicked use me");
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-slots/src/components/AppButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button @click="useMeChild" v-on="$listener">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppButton',
+        methods: {
+            useMeChild() {
+                console.log("Clicked Use me from Child");
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    button {
+        color: white;
+        background: green;
+        font-size: 22px;
+        border: none;
+        appearance: none;
+        margin: 20px;
+        padding: 20px;
+        &:hover {
+            box-shadow: 0 0 0 10px green;
+        }
+    }
+</style>
